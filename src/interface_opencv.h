c14172575267ee0f1ef06347ea1fb54e89c30c4a
Li Ge <lge@ucsd.edu>
2013-07-30 08:48:42 -0700

    add legacy to header

--- src/interface_opencv.h
+++ src/interface_opencv.h
@@ -2,17 +2,17 @@
 
   Image-based 3d modelling software
   Copyright (C) 2007-2008  Lukas Mach
-  
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,13 +25,14 @@
 #include "pthread.h"
 #include "cv.h"
 #include "highgui.h"
+#include "opencv2/legacy/legacy.hpp"
 
 const double OPENCV_PI = 3.14159265358979323846;
 
 // macro for accessing double precision matrix elements // todo avoid using OpenCV macro
 #define OPENCV_ELEM(matrix, i, j) (CV_MAT_ELEM((*(matrix)), double, (i), (j)))
 
-// thread safety 
+// thread safety
 void opencv_begin();
 
 // thread safety
@@ -46,24 +47,24 @@ CvMat * opencv_create_matrix(const size_t rows, const size_t cols, double data[]
 // new identity matrix
 CvMat * opencv_create_I_matrix(const size_t n);
 
-// create double precision column vector from array of doubles 
+// create double precision column vector from array of doubles
 CvMat * opencv_create_vector(const double a[], const size_t length);
 
-// x axis rotation matrix 
-// todo optimize 
-// note these matrices are transposes of conventional mathematical rotation matrices 
+// x axis rotation matrix
+// todo optimize
+// note these matrices are transposes of conventional mathematical rotation matrices
 // (which is ugly, but it just so happens, that we need it this way)
 CvMat * opencv_create_rotation_matrix_x(const double angle);
 
-// y axis rotation matrix 
+// y axis rotation matrix
 // todo optimize
 CvMat * opencv_create_rotation_matrix_y(const double angle);
 
-// z axis rotation matrix 
+// z axis rotation matrix
 // todo optimize
 CvMat * opencv_create_rotation_matrix_z(const double angle);
 
-// euler angles to rotation matrix 
+// euler angles to rotation matrix
 // todo optimize
 CvMat * opencv_create_rotation_matrix_from_euler(const double euler[], bool reverse_order = true);
 
@@ -76,10 +77,10 @@ void opencv_vertex_projection_visualization(const CvMat * const P, const double 
 // project vertex represented by homogeneous vector - used only for visualization purposes
 void opencv_vertex_projection_visualization(const CvMat * const P, const CvMat * X, double reprojection[]);
 
-// decompose rotation matrix into euler angles 
+// decompose rotation matrix into euler angles
 void opencv_rotation_matrix_to_angles(const CvMat * const R, double & ax, double & ay, double & az);
 
-// debug function prints matrix to standard output 
+// debug function prints matrix to standard output
 void opencv_debug(const char * title, CvMat * A);
 
 // downsize image
@@ -88,7 +89,7 @@ void opencv_downsize(IplImage ** img, const int max_size);
 // create copy scaled down below some width threshold
 IplImage * opencv_downsize_copy(IplImage * img, const int max_size);
 
-// load image and scale it down below some width threshold 
+// load image and scale it down below some width threshold
 IplImage * opencv_load_image(const char * filename, const int max_size);
 
 // creates trivial image
@@ -97,22 +98,22 @@ IplImage * opencv_create_substitute_image();
 // creates image with sides of the form 2^n
 IplImage * opencv_create_exp_image(const int width, const int height, const int depth, const int channels);
 
-// calculate right null-vector of matrix A 
+// calculate right null-vector of matrix A
 CvMat * opencv_right_null_vector(CvMat * A);
 
 // calculate left null-vector of matrix A
 CvMat * opencv_left_null_vector(CvMat * A);
 
-// create cross product matrix for 3-vector x 
+// create cross product matrix for 3-vector x
 CvMat * opencv_create_cross_product_matrix(CvMat * x);
 
 // scale vector homogeneous vector so that it's last coordinate is 1
 void opencv_rescale_homogeneous_vector(CvMat * X);
 
-// normalize vector 
+// normalize vector
 void opencv_normalize(CvMat * x);
 
-// normalize vector 
+// normalize vector
 void opencv_normalize_inhomogeneous(CvMat * x);
 
 // normalize vector
